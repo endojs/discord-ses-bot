2fd43dc016c21b4e4f0011b04294869e4074436c
kumavis <aaron@kumavis.me>
2021-04-06 20:52:01 +0800

    git switch

--- .github/workflows/deploy-remote.sh
+++ .github/workflows/deploy-remote.sh
@@ -3,7 +3,7 @@ echo "updating server"
 cd /root/project/discord-ses-bot
 . ~/.nvm/nvm.sh
 
-git fetch origin && git checkout $GITHUB_REF
+git fetch origin && git switch $GITHUB_REF
 docker-compose build && docker-compose stop && docker-compose up -d
 # yarn setup
 # yarn clearDb
