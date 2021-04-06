0e2203a20a4d2917592261e0f282df9d3e14c066
kumavis <aaron@kumavis.me>
2021-04-06 20:58:06 +0800

    x

--- .github/workflows/deploy-remote.sh
+++ .github/workflows/deploy-remote.sh
@@ -3,5 +3,7 @@ echo "updating server"
 cd /root/project/discord-ses-bot
 . ~/.nvm/nvm.sh
 
+echo "switching to $GITHUB_REF"
+
 git fetch origin && git switch $GITHUB_REF
 docker-compose build && docker-compose stop && docker-compose up -d
--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:14
 
 COPY {package*.json,yarn.lock} ./
-RUN yarn install
+RUN yarn install --frozen-lockfile
 
 COPY . .
 RUN yarn prod
