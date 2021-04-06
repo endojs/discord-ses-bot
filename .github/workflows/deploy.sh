6d0c633c3074dd54f1f1650ea77349b95e9b04c2
kumavis <aaron@kumavis.me>
2021-04-06 16:50:31 +0800

    idk- linux was a mistake

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,6 +1,11 @@
 echo "$SSH_PRIVATE_KEY" > key.txt
 chmod 600 key.txt
+
+echo 'PermitUserEnvironment yes' >> /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
 ssh -i key.txt \
+  -o PermitUserEnvironment=yes \
   -o StrictHostKeyChecking=no \
-  "$REMOTE_USER@$REMOTE_HOST" 'bash -s' \
+  "$REMOTE_USER@$REMOTE_HOST" 'bash --login -s' \
   < .github/workflows/deploy-remote.sh
