285da100d5aedb95b62f363dbdb985469f0aa1a0
kumavis <aaron@kumavis.me>
2021-04-06 20:49:23 +0800

    mutable ref

--- .github/workflows/deploy-remote.sh
+++ .github/workflows/deploy-remote.sh
@@ -3,7 +3,7 @@ echo "updating server"
 cd /root/project/discord-ses-bot
 . ~/.nvm/nvm.sh
 
-git pull origin main
+git fetch origin && git checkout $GITHUB_REF
 docker-compose build && docker-compose stop && docker-compose up -d
 # yarn setup
 # yarn clearDb
