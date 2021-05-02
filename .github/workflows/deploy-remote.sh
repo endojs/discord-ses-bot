509c225af24a67277756e83700c0c413e52164ef
kumavis <aaron@kumavis.me>
2021-05-02 22:11:19 +0800

    cd - deploy to k8s

--- .github/workflows/deploy-remote.sh
+++
@@ -1,9 +0,0 @@
-# docker stop
-echo "updating server"
-cd /root/project/discord-ses-bot
-. ~/.nvm/nvm.sh
-
-echo "switching to $GITHUB_SHA"
-
-git fetch origin && git checkout $GITHUB_SHA
-docker-compose build && docker-compose stop && docker-compose up -d
--- .github/workflows/deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "$SSH_PRIVATE_KEY" > key.txt
-chmod 600 key.txt
-
-ssh -i key.txt \
-  -o StrictHostKeyChecking=no \
-  "$REMOTE_USER@$REMOTE_HOST" "GITHUB_SHA=$GITHUB_SHA bash --login -s" \
-  < .github/workflows/deploy-remote.sh
