54f55548f728731c9c1d299f0dd0a94720e50718
kumavis <aaron@kumavis.me>
2021-10-31 00:35:05 -0700

    small refactor for client start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { appKey, appToken } = require('./config.json')
 const {
   createMachine
 } = require('./machine')
-const { Client } = require('discord.js')
+const { Client: DiscordClient } = require('discord.js')
 
 // This int isn't sensitive, it just describes the permissions we're requesting:
 const PERMISSIONS_INT = 2147503168
@@ -11,26 +11,35 @@ const link = `https://discord.com/oauth2/authorize?client_id=${appKey}&scope=bot
 main()
 
 async function main () {
-  console.log(`Starting SES-bot! Add to your discord server with this link: \n${link}`)
+  console.log(`Starting SES-bot!`)
 
   /**
    * DISCORD CONFIG SECTION
    */
-  const client = new Client({
+  console.log(`Add to your discord server with this link: \n${link}`)
+  const discord = new DiscordClient({
     client_id: appKey,
     scope: 'bot',
     permissions: PERMISSIONS_INT
   })
-
-  client.login(appToken)
-  const machine = createMachine()
+  discord.login(appToken)
+  discord.on('message', (discordMsg) => {
+    handleMessage({
+      authorId: discordMsg.author.id,
+      content: discordMsg.content,
+      reply: discordMsg.reply,
+    })
+  })
 
   /**
-   * CHAT MESSAGE HANDLING
+   * MESSAGE HANDLING
    */
+  const machine = createMachine()
 
-  client.on('message', async msg => {
-    const authorId = msg.author.id
+  await machine.replayPastFromDisk()
+
+  async function handleMessage (msg) {
+    const authorId = msg.authorId
     const message = msg.content
 
     const simulatePrefix = '?'
@@ -56,7 +65,6 @@ async function main () {
     }
 
     machine.queue({ loggable, msg })
-  })
+  }
 
-  await machine.replayPastFromDisk()
 }
