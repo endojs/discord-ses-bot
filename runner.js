f98ce5e8ab32c3542c64c5ccc69c1c138ec58907
kumavis <aaron@kumavis.me>
2022-10-28 16:14:08 -1000

    log initialization

--- runner.js
+++ runner.js
@@ -24,6 +24,7 @@ export function createRunner () {
     })
     const kernelSrc = await fs.readFile(path.join(__dirname, 'kernel.js'), 'utf8')
     await worker.evaluate(kernelSrc)
+    console.log('initialized kernel')
     // await worker.evaluate(`handleCommand = (function(){\n${kernelSrc}\n})()`);
     isInitialized = true
   }
